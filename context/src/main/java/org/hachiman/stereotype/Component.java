2eb54d3209ee8563fdfc1ec0a634a4c831d421ef
liujiangpeng <liujiangpeng@hjfruit.com>
2023-09-20 21:01:09 +0800

    feat:init

---
+++ context/src/main/java/org/hachiman/stereotype/Component.java
@@ -0,0 +1,17 @@
+package org.hachiman.stereotype;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * annotation for component
+ *
+ * @author ljp
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+public @interface Component {
+
+}
